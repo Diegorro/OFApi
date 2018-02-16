4e8db110b8d96b6f844945e73e59a61c74039d61
diego <32820519+DIEGO32OF@users.noreply.github.com>
2018-02-15 18:38:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 var mongoose = require('mongoose');
 var app=require('./app');
-//var Socket=require('./socketcomanda');
 
 
 
-var port=process.env.PORT||3977;
+
+
 mongoose.connect('mongodb://DiegoR:SALINIDAD1*@ds231568.mlab.com:31568/heroku_7xdfk9c8', (err,res) => {
     if(err)
     {
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://DiegoR:SALINIDAD1*@ds231568.mlab.com:31568/heroku_7x
 else
 {
     console.log('Register Bd correct');
-   // app.listen(port,function(){console.log('Server is run correct port:'+port);});
+ 
 }
 });
 
