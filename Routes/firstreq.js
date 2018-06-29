5a439310eb018880f397a0047e9f14de66385a18
diego <32820519+DIEGO32OF@users.noreply.github.com>
2018-06-29 18:33:18 -0500

    Update firstreq.js

--- Routes/firstreq.js
+++ Routes/firstreq.js
@@ -7,4 +7,9 @@ var api = expres.Router();
 
 api.post('/getordereasy/:typer/:Esta', SolicitudPrimera.GetInfo);
 api.post('/VerifyCode/:code/:Local', SolicitudPrimera.VerifyCode);
+api.post('/CheckOptions/:hash/:numericSet', SolicitudPrimera.makeToken);
+api.post('/searchGet/:typer/:Busqueda', SolicitudPrimera.GetBusca);
+api.post('/Vigenciacheck/:Token/', SolicitudPrimera.validateToken);
+
+
 module.exports = api;
