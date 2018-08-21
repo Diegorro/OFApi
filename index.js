a6b1ae69341442a94c836e4853c1a58c12f82ac6
diego <32820519+DIEGO32OF@users.noreply.github.com>
2018-08-21 15:28:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var app=require('./app');
 
 var port=process.env.PORT||3977;
 
-mongoose.connect('mongodb://DiegoR:SALINIDAD1*@ds231568.mlab.com:31568/heroku_7xdfk9c8', (err,res) => {
+//mongoose.connect('mongodb://DiegoR:SALINIDAD1*@ds231568.mlab.com:31568/heroku_7xdfk9c8', (err,res) => {
+mongoose.connect('mongodb://DiegoR:DEFTONES1*@dds245210.mlab.com:45210/heroku_bql92j6t', (err,res) => {
     if(err)
     {
     throw err;
