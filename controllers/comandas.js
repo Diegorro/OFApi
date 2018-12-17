0cd6743cfd3f0a4a68e5d2d4f9f37bd197ee46b0
Diegorro <que.nuevo@hotmail.com>
2018-12-17 17:28:43 -0600

    Update comandas.js

--- controllers/comandas.js
+++ controllers/comandas.js
@@ -96,7 +96,7 @@ function GetComandByCode(req,res){
     console.log('x aca');
   console.log(codigo+'---'+Local);
   if(codigo!=''){
-      var Codigoget = comanda.findOne({ codigoStr:  codigo.toLowerCase(),local:Local  },(err,ComandFounder)=>{
+      var Codigoget = comanda.findOne({ codigoStr:  new RegExp(codigo, 'i'),local:Local  },(err,ComandFounder)=>{
           if (err)
               res.status(500).send({ message: 'Error en Peticion' });
           else {
